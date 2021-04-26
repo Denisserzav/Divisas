9e142605b1b0ce5b88c6cd448f39b6c6570d818a
Denisserzav <karendenir@gmail.com>
2021-04-26 01:45:30 -0500

    Update divisa.c
    correccion de errores

--- divisa.c
+++ divisa.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
 int main(){
-    float a = 0, b = 0 resultado = 0;
-    int operacion;
+    float a = 0, b = 0, resultado = 0;
+    int opcion;
     printf("¿A que moneda deseas converitir?");
     printf("1.- Dolar \n 2.- Libra \n 3.-Euro");
     
     scanf ("%d", &opcion);
-    switch(operacion){
+    switch(opcion){
         case 1:
          printf("Dame la cantidad de pesos");
-         scanf(%f, &a);
-         (operacion b== 20)
+         scanf("%f", &a);
+         (operacion "b=20");
          print ("El resultado de %O.2f dolares a %O.2F PesosMX es ", a, b, a * b);
          break;
         case 2:
          printf("Dame la cantidad de pesos");
-         scanf (%f, &a);
-         (operacion b==18)
-         printf ("El resultado de %O.2f libras a %O.2F MX es ", a, b, a * b);
+         scanf ("%f", &a);
+         (operacion b==18);
+         printf ("El resultado de %O.2f libras a %O.2F PesosMX es ", a, b, a * b);
          break;
         case 3:
          printf("Dame la cantidad de pesos");
-         scanf (%f, &a);
-         (operacion b==19)
-         printf ("El resultado de %O.2f euros a %O.2F MX es ", a, b, a * b);
+         scanf ("%f", &a);
+         (operacion b==19);
+         printf ("El resultado de %O.2f euros a %O.2F PesosMX es ", a, b, a * b);
 
     
 
